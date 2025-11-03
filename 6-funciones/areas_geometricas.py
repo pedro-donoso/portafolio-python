5065717479565eba1c963e6093035a17afde90ea
pedro-donoso <pedro.donoso.r@usach.cl>
2025-11-03 14:43:26 -0300

    conversor temperatura

---
+++ 6-funciones/areas_geometricas.py
@@ -0,0 +1,14 @@
+def conversor_unidades():
+    print("CONVERSOR DE UNIDADES")
+    
+    while True:
+        print("\n1. Temperatura\n2. Monedas\n3. Longitudes\n4. Salir")
+        opcion = input("Elige: ")
+        
+        if opcion == '1':
+            temp = float(input("Grados Celsius: "))
+            fahrenheit = (temp * 9/5) + 32
+            kelvin = temp + 273.15
+            print(f"{temp}ºC = {fahrenheit:.1f}ºF = {kelvin:.1f}K")
+            
+            
